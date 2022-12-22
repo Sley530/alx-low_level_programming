df65627e6b4271e10892a9fe02ed2f8e2f48f4ea
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2022-12-22 02:07:19 -0500

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Starting point for the program execution
  *
- * Description: Prints with a newline "with proper grammar, but the outcome is a piece of art,"
- * 
+ * Description: Prints with a newline the above string
  * Return: Always return 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
