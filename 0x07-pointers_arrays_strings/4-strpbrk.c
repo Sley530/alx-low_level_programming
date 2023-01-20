fb0ab36aab330140b3a63a532c1c818394a7fafb
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-19 23:29:36 -0500

    Update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * @s: the string
  * @accept: set of bytes
  *
- * Return: a pointer to the byte in s that matches one of the bytes in accept
+ * Return: a pointer to the byte in s that matches one of the bytes in accept.
  */
 
 char *_strpbrk(char *s, char *accept)
