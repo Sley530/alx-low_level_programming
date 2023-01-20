343af7dba0c4e6c409c636a749b3f96b487b4dfb
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-19 23:29:41 -0500

    Update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,7 @@
  * @needle: substring to locate
  *
  * Return: pointer to the beginning of the located substring,
- * or NULL, if substring is not found
+ * or NULL, if substring is not found.
  */
 
 char *_strstr(char *haystack, char *needle)
