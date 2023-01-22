719f90f69e9e40c2a03e924e8c81ab2798df04e6
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-21 23:36:59 -0500

    Update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
  * @n: unsigned long int input.
  * @index: index of the bit.
  *
- * Return: value of the bit.
+ * Return: value of the bit
  */
 
 int get_bit(unsigned long int n, unsigned int index)
