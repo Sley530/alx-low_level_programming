ab797b0b066c3719f5e022a04ea3b87ac8532dd0
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-21 23:37:08 -0500

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,8 @@
  * @filename: filename.
  * @letters: numbers of letters printed.
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: numbers of letters printed. It fails,
+ * returns 0.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
