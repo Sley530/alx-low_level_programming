088d9ae22a037758931d3f11b89caeda4ffbe6a9
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-21 23:35:58 -0500

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
- * Return: number of elements in the list.
+ * Return: number of elements in the list
  */
 
 size_t print_list(const list_t *h)
