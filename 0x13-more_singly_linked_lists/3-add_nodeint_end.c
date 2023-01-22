037efd5a62047237b030eb523f7f4f05dbf0769f
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-21 23:36:27 -0500

    Update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,7 +6,8 @@
  * @head: head of a list.
  * @n: n element.
  *
- * Return: address of the new element. NUll if it failed.
+ * Return: address of the new element.
+ * NUll if it failed.
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
